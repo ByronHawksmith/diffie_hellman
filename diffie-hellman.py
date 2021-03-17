c42eeb904b4f65c83060bf7365bd921f050ba865
Byron Hawksmith <hawksmithbyron@tutanota.com>
2021-03-17 18:53:59 +1030

    Add Comments

--- diffie-hellman.py
+++ diffie-hellman.py
@@ -35,10 +35,14 @@ b = 958  # server priv key
 P = 3213  # client pub key
 a = 5  # client priv key
 
+# client is only aware of it's public key, it's private key and the servers public key
 x = client_step_one(P, a, G)
+# server is only aware of it's public key, it's private key and the clients public key
 y = server_step_one(G, b, P)
 
+# client computes the shared key based on the servers calculation of y
 key_a = client_step_two(y, a, P)
+# server computes the shared key based on the clients calculation of x
 key_b = server_step_two(x, b, P)
 
 print(key_a)
